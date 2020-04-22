1b3d30094883dc666e71e5dff66af4efec74a588
Aaron Williams <aaronrw4@yahoo.com>
2020-04-21 23:22:43 -0700

    Added button and title

--- schemes/src/Components/home.js
+++ schemes/src/Components/home.js
@@ -19,6 +19,8 @@ function Home() {
 
     return(
         <div>
+            <h1>Schemes Home Page</h1>
+            <button>Add Scheme</button>
             {schemes.map(scheme => (
                 <div key={scheme.id}>
                     <p>{scheme.scheme_name}</p>
