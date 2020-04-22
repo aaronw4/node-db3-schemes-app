0edb3e7956d4089b86b33bbb4d5786777e189bff
Aaron Williams <aaronrw4@yahoo.com>
2020-04-21 22:56:01 -0700

    imported Home into app page

--- schemes/src/App.js
+++ schemes/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './Components/home'
 
 function App() {
   return (
     <div className="App">
-      Watup
+      <Home/>
     </div>
   );
 }
